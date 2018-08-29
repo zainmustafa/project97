6a89b77714f78873d9194e7e1d8372d702e934b4
aselleq <assel.qp@gmail.com>
2018-08-29 18:01:54 -0400

    lala

--- app/components/addNewCampus.js
+++ app/components/addNewCampus.js
@@ -14,7 +14,16 @@ export default class addNewCampus extends Component {
             value="campusName"
             placeholder="enter Campus Name"
           />
+          Campus Address{" "}
+          <input
+            type="text"
+            name="campusAddress"
+            value="campusAddress"
+            placeholder="enter Campus Address"
+          />
           <imput type="submit" name="addCampus" value="Submit" />
+          {/* line 84 onSubmit should: Make an AJAX request that causes the new campus to be persisted in the database */}
+          {/* line 85 Add the new campus to the list of campuses without needing to refresh the page */}
         </form>
       </div>
     );
